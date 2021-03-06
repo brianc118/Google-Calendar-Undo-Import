fcf34b2efb02ccbf79b9d8b1158fe73cf4b951a5
Mai Le <male6657@uni.sydney.edu.au>
2021-03-06 11:41:38 +1100

    update user authentication

--- gcal_undo_import.py
+++ gcal_undo_import.py
@@ -44,7 +44,26 @@ def getUserInputIndex(msg, i):
     return x
 
 def main(argv):
-    # Authenticate and construct service.
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)# Authenticate and construct service.
     service, flags = sample_tools.init(
         argv, 'calendar', 'v3', __doc__, __file__,
         scope='https://www.googleapis.com/auth/calendar')
